022a494dd9b6df5df3bf23cb0ea40353c9ac8ce4
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2022-10-02 00:12:03 -0500

    added movies.js

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const axios = require("axios");
 //modules section
 const handleWeather = require("./weather.js");
 
+const handleMovies = require("./movies.js");
+
 // Middleware
 //The app.use() function is used to mount the specified middleware function(s) at the path which is being specified
 app.use(cors());
@@ -32,37 +34,15 @@ app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 //--------------------------
 app.get("/weather", handleWeather);
 
-class Movies {
-  constructor(title) {
-    this.title = title;
-  }
-}
+app.get("/movies", handleMovies);
 
 app.get("/", (req, res) => {
   res.send("Hello from the home route!");
 });
 
 //getting movies onto backend site
-app.get("/movies", async (req, res) => {
-  try {
-    //ApI key, make a request to movie database
-
-    const API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${req.query.searchQuery}`;
-
-    let movieResults = await axios.get(API);
-    //response needed later
-    movieResults = movieResults.data.results.map((movie) => {
-      return new Movies(movie.title);
-    });
-
-    res.send(movieResults);
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 // Catch all endpoints
-
 app.get("*", (req, res) => {
   res.status(404).send("Page not Found");
 });
