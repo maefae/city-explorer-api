c5e464eb8c08e8db36ad545ac2dc7d6bfb9d947e
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2022-10-02 22:20:04 -0500

    modified movies.js

--- movies.js
+++ movies.js
@@ -3,6 +3,9 @@
 require("dotenv").config();
 
 const axios = require("axios");
+const { request } = require("express");
+
+let cache = require("./cache.js");
 
 class Movies {
   constructor(title) {
@@ -15,16 +18,26 @@ async function handleMovies(req, res) {
     //ApI key, make a request to movie database
 
     const API = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${req.query.searchQuery}`;
+    const key = "movie-" + req.query.searchQuery;
 
-    let movieResults = await axios.get(API);
-    //response needed later
-    movieResults = movieResults.data.results.map((movie) => {
-      return new Movies(movie.title);
-    });
+    if (cache[key] && Date.now() - cache[key].timestamp < 50000) {
+      console.log("Cache hit");
+    } else {
+      console.log("Cache miss");
+      cache[key] = {};
+      cache[key].timestamp = Date.now();
 
-    res.send(movieResults);
+      let movieResults = await axios.get(API);
+      //response needed later
+      cache[key].data = movieResults.data.results.map((movie) => {
+        return new Movies(movie.title);
+      });
+    }
+
+    res.send(cache[key].data);
   } catch (error) {
     console.log(error);
   }
 }
+
 module.exports = handleMovies;
